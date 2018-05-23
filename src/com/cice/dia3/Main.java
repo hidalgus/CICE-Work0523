97c3a812b90f9c06f91930ada56d5fb5eb56901e
Morales Hidalgo <smorales@indra.es>
2018-05-23 21:22:30 +0200

    cambio

--- src/com/cice/dia3/Main.java
+++ src/com/cice/dia3/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         //venga vamos a hacer un cambio para ver como chuta GIT
-        System.out.printf("Hola proyecto nuevo con cambio2");
+        System.out.printf("Hola proyecto ");
     }
 }
