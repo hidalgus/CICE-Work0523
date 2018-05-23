a438456b4ba67caacc2b6ed3e0218ca17fcb7128
Morales Hidalgo <smorales@indra.es>
2018-05-23 21:15:59 +0200

    first commit

---
+++ src/com/cice/dia3/Main.java
@@ -0,0 +1,10 @@
+package com.cice.dia3;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //venga vamos a hacer un cambio para ver como chuta GIT
+        System.out.printf("Hola proyecto nuevo con cambio2");
+    }
+}
